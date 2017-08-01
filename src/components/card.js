56556d14647cd72ab3603d100944d47159748702
Varik <varikmatevosyan@gmail.com>
2017-08-01 14:18:13 +0400

    some changes

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,8 @@ import {
     View,
     Image,
     Animated,
-    TouchableOpacity
+    TouchableOpacity,
+    PanResponder
 } from 'react-native';
 
 import Styles from '../styles';
@@ -11,11 +12,21 @@ import Styles from '../styles';
 export default class Home extends Component {
     constructor(props) {
         super(props);
+
+        this.panResponder = PanResponder.create({
+            // Ask to be the responder:
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onPanResponderRelease: this.props.moveCard
+        });
+
     }
 
     render() {
         return (
-                <Animated.Image source={this.props.sprite} style={[Styles.card, this.props.spriteStyle]} />
+                <Animated.Image source={this.props.sprite} style={[Styles.card, this.props.spritePosition, {transform: [{scale: this.props.spriteScale}]}]} {...this.panResponder.panHandlers}/>
         )
     }
 }
--- src/components/deck.js
+++ src/components/deck.js
@@ -4,10 +4,9 @@ import {
     Image,
     Dimensions,
     Animated,
-    TouchableNativeFeedback,
     TouchableOpacity,
-    TouchableHighlight,
-    PanResponder
+    PanResponder,
+    Text
 } from 'react-native';
 
 import Config from '../config';
@@ -23,24 +22,29 @@ export default class Deck extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cardPositions:  _.map(_.range(32), i => new Animated.ValueXY({x, y: y + i * .5}))
+            cardPositions:  _.map(_.range(32), i => new Animated.ValueXY({x, y: y + i * .5})),
+            showButton: true,
+            cardScales: _.map(_.range(32), i => 1)
         };
 
         this.panResponder = PanResponder.create({
             // Ask to be the responder:
-
-            onPanResponderRelease: ()=>alert(1),//this.distribute.bind(this),
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onPanResponderRelease: this.moveCard.bind(this),
+            onPanResponderGrant: this.scaleCard.bind(this),
+            onPanResponderMove: this.dragCard.bind(this)
         });
 
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.distribute();
-        }, 500)
     }
 
     distribute(e) {
+        this.setState({showButton: false});
         let cardX = width / 2 - 4 * 50, cardY;
         _.each(_.range(16), i => {
             cardX += 20;
@@ -58,19 +62,31 @@ export default class Deck extends Component {
         })
     }
 
-    cardPress(index) {
+    moveCard(index) {
         if(!CARDS[index].distributed) return;
         Config.cardSprites[index] = '7';
+        let minusY =  index % 2 == 0?  40: -5;
         Animated.spring(this.state.cardPositions[index], {
-            toValue: {x: width / 2 - 35, y: height / 2 - 25}
+            toValue: {x: width / 2 - 35, y: height / 2 - minusY}
         }).start();
+    }
+
+    scaleCard(e, ) {
+        this.setState({})
+    }
+
+    dragCard(e) {
+
     }
 
     render() {
         return (
-            <View activeOpacity={.9} {...this.panResponder.panHandlers}>
+            <View activeOpacity={.9}>
+                {this.state.showButton? (<TouchableOpacity onPress={this.distribute.bind(this)} style={{left: width / 2 - 62, top: height / 2 - 25, position: 'absolute', backgroundColor: 'white'}}>
+                    <Text style={{fontSize: 24, padding: 10}}>Distribute</Text>
+                </TouchableOpacity>): null}
                 {this.props.cards.map((key, index) => {
-                    return <Card sprite={Config.cardSprites[key]} key={index} spriteStyle={this.state.cardPositions[index].getLayout()} panHandlers={this.panResponder.panHandlers}/>
+                    return <Card sprite={Config.cardSprites[key]} key={index} spritePosition={this.state.cardPositions[index].getLayout()} spriteScale={this.state.cardScales[index]} moveCard={this.moveCard.bind(this, index)}/>
                 })}
             </View>
         )

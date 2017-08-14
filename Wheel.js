d280224390b3f40b024d1e69d41d27cee76035f8
Andrey <andrey.zaq1@gmail.com>
2017-08-14 12:15:54 +0400

    wheel v2.1

--- Wheel.js
+++ Wheel.js
@@ -1,4 +1,54 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var imageId = [];
+var number = [];
+var prize;
+var wedgeSprite = [];;
+var wedgeGroup;
+var startAngle = 0;
+var i,c=-1;
+var text =[];
+var example = 	[{amount:3, color:"yellow"},
+				{amount:200, color:"red"},
+				{amount:300, color:"red"},
+				{amount:5, color:"yellow"},
+				{amount:500, color:"red"},
+				{amount:600, color:"red"},
+				{amount:10, color:"yellow"},
+				{amount:800, color:"red"},
+				{amount:100, color:"red"},
+				{amount:25, color:"yellow"},
+				{amount:400, color:"red"},
+				{amount:700, color:"red"}];
+				
+class Wheel{
+	constructor(arg){
+		imageId[0]="wedge_"+arg[0].color;
+		for(var j = 11; j >= 0; j--){
+			imageId[12-j]="wedge_"+arg[j].color;
+			number[j]=arg[j].amount;
+		}
+	}
+}
+
+Wheel.prototype.addSegment = function(){
+	wedgeGroup.angle = Math.round(startAngle);
+    wedgeSprite[i] = game.make.sprite(0, 0, imageId[i]);
+    wedgeSprite[i].angle = c-1; 
+    wedgeSprite[i].anchor.setTo(-0.1, 0.5);
+    wedgeSprite[i].scale.setTo(1, 1);
+    wedgeGroup.add(wedgeSprite[i]);
+    c+=30;
+}
+
+Wheel.prototype.addText = function(){
+		var style = { font: "32px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 1000};
+    	text[i] = game.add.text(0, 0, "													"+number[i], style);
+   		text[i].anchor.set(0, 0.5);
+   		wedgeGroup.add(text[i]);
+   		text[i].angle = Math.round(startAngle);
+    	startAngle = Math.round(startAngle+30);
+    	c+=30;
+	}
 
 function preload() {
 	game.load.image('wheel', 'assets/center_circle_360.png');
@@ -8,17 +58,10 @@ function preload() {
 	game.load.image('circle', 'assets/circle-spin.png');
 	game.load.image('wedge_yellow', 'assets/wedge_yellow_360.png');
 }
-var prize;
-var imageId = [];
-var wedgeSprite = [];
-var firstNumber = [];
-var secondNumber = [];
-var time = new Date();
-var wedgeGroup;
-var startAngle = 0;
-var i,c=-1;
-var text =[];
+
+var w = new Wheel(example);
 function create() {
+	
 	wedgeGroup = game.make.group();
 	var wheel = game.add.sprite(0, 0, 'wheel');	
 	var spinner = game.add.sprite(700, 100, 'spinner');
@@ -31,11 +74,11 @@ function create() {
 	arrow.anchor.setTo(0.5,0.5);
 	arrow.scale.setTo(0.2,0.2);
 	for(i = 0;i < 12;i++){
-		addSegment();   	
+		Wheel.prototype.addSegment(); 	
 	}
 	startAngle =0;
 	for(i = 0;i < 12;i++){
-		addText();   	
+		Wheel.prototype.addText();   	
 	}
 	var wheel1 = game.add.sprite(0, 0, 'circle');
 	wedgeGroup.add(wheel1);
@@ -44,40 +87,19 @@ function create() {
 	wedgeGroup.angle = 16.5-90;
 	spinner.inputEnabled = true;
 	spinner.events.onInputDown.add(function(){
-	spinner.destroy();
-	var tween = game.add.tween(wedgeGroup);
-	var random = 360 * Math.random();
-	var a = -3616.5-90 + random;
-	var slot = Math.floor(((-a+16.5-90)%360)/30);
-	tween.to({angle: a}, 7000, Phaser.Easing.Circular.InOut, true, 0);
-	setTimeout(function(){alert( "You win " + (firstNumber[slot]*100+secondNumber[slot]*10)+" chips!")},7010);
-});
-}
-function addSegment(){
-	wedgeGroup.angle = Math.round(startAngle);
-
-		if(Math.random()>0.5){
-        	imageId[i] = "wedge_red";
-   		}
-    	else{
-       		imageId[i] = "wedge_yellow";
-    	}
-    	wedgeSprite[i] = game.make.sprite(0, 0, imageId[i]);
-    	wedgeSprite[i].angle = c-1; 
-    	wedgeSprite[i].anchor.setTo(-0.1, 0.5);
-    	wedgeSprite[i].scale.setTo(1, 1);
-    	wedgeGroup.add(wedgeSprite[i]);
-    	c-=30;
-}
-
-function addText(){
-	var style = { font: "32px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 10000};
-	firstNumber[i]=Math.floor(Math.random()*9+1);
-	secondNumber[i]=Math.floor(Math.random()*10);
-    	text[i] = game.add.text(0, 0, "													"+firstNumber[i]+secondNumber[i]+0, style);
-   		text[i].anchor.set(0, 0.5);
-   		wedgeGroup.add(text[i]);
-   		text[i].angle = Math.round(startAngle);
-    	startAngle = Math.round(startAngle+30);
-    	c-=30;
+		spinner.inputEnabled = false;
+		var tween = game.add.tween(wedgeGroup);
+		var random = 360 * Math.random();
+		var a = -3616.5-90 + random;
+		var slot = Math.floor(((-a+16.5-90)%360)/30);
+		tween.to({angle: a}, 5000, Phaser.Easing.Circular.InOut, true, 0);
+		setTimeout(function(){
+			if(imageId[slot]==="wedge_red"){
+			alert( "You win " + number[slot]+" chips!");
+			}
+			else{
+				alert( "You win " + number[slot]+" gold chips!");
+			}
+		},5010);
+	});
 }
